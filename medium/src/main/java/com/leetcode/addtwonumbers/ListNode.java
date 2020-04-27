8e77f642594733421477e69700e36bfef37aab40
nikolay chechenko <oxibex@gmail.com>
2020-04-27 15:35:18 +0300

    add two numbers

---
+++ medium/src/main/java/com/leetcode/addtwonumbers/ListNode.java
@@ -0,0 +1,17 @@
+package com.leetcode.addtwonumbers;
+
+/**
+ * @author Nikolay Chechenko.
+ * @since 27.04.2020
+ */
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
